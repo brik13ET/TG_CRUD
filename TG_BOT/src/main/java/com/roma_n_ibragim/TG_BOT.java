d6a500e415d2544c351f3a0765c7239de1b00378
brik13ET <aleev.ibragim0909@gmail.com>
2023-04-19 06:09:44 +0400

    pomed

---
+++ TG_BOT/src/main/java/com/roma_n_ibragim/TG_BOT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.roma_n_ibragim;
+
+/**
+ *
+ * @author user0
+ */
+public class TG_BOT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
